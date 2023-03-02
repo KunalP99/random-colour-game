23da773a9ad0d77ae509d0ec19359ea77e154b8c
KunalP99 <kunal_patel200@hotmail.co.uk>
2023-03-02 19:17:09 +0000

    feat: generate random colour on load

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/app.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const hexArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+  const [correctHex, setCorrectHex] = useState('');
+
+  const getRandomHex = () => {
+    const hex = [];
+    for (let i = 0; i < 6; i++) {
+      hex.push(hexArr[Math.floor(Math.random() * (15 - 0 + 1)) + 0]);
+    }
+    const hexString = hex.toString().replaceAll(',', '');
+    return `#${hexString}`;
+  }
+
+  useEffect(() => {
+    setCorrectHex(getRandomHex);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="box" onClick={getRandomHex} style={{background: `${correctHex}`}}></div>
+      <button>{correctHex}</button>
     </div>
   );
 }
