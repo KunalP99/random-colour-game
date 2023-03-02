6e460ac27a8ad31806956e85fcc9b6563c0d248d
KunalP99 <kunal_patel200@hotmail.co.uk>
2023-03-02 20:18:50 +0000

    feat: add 3 buttons, one being the correct hex and others being incorrect hex

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react';
 function App() {
   const hexArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
   const [correctHex, setCorrectHex] = useState('');
+  const [hexArray, setHexArray] = useState([]);
 
   const getRandomHex = () => {
     const hex = [];
@@ -12,16 +13,32 @@ function App() {
     }
     const hexString = hex.toString().replaceAll(',', '');
     return `#${hexString}`;
-  }
+  };
 
   useEffect(() => {
-    setCorrectHex(getRandomHex);
+    const answer = getRandomHex();
+    setCorrectHex(answer);
+    setHexArray([answer, getRandomHex(), getRandomHex()].sort(() => 0.5 - Math.random()));
   }, []);
+
+  const handleClick = (e) => {
+    console.log(e.target.innerText);
+    if (e.target.innerText === correctHex) {
+      console.log('CORRECT');
+    } else {
+      console.log('INCORRECT');
+      e.target.style.backgroundColor = 'red';
+    }
+  }
 
   return (
     <div className="App">
-      <div className="box" onClick={getRandomHex} style={{background: `${correctHex}`}}></div>
-      <button>{correctHex}</button>
+      <div className="box" style={{background: `${correctHex}`}}></div>
+      <div className='btn-container'>
+      {hexArray && hexArray.map(hex => {
+        return <button onClick={handleClick} key={hex}>{hex}</button>
+      })}
+      </div>
     </div>
   );
 }
